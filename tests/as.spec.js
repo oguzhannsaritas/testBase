26382a4892da7ebb61b8eb4a606011299632e968
oguzhannsaritas <oguzhan_saritass@hotmail.com>
2025-03-19 06:47:19 +0300

    Editing and adding screen shots 6

--- tests/as.spec.js
+++ tests/as.spec.js
@@ -2,7 +2,7 @@
 import { test, expect, chromium } from '@playwright/test';
 import { customClick, customFill, customWaitForTimeout, customLocator, waitForLoadState, customPlaceholder } from '../utilities/utilities.js';
 
-test('aaa', async ({  }) => {
+test('as', async ({  }) => {
  const browser = await chromium.launch({
         headless: true,
         args: ['--window-position=0,0', '--window-size=1920,1080']
@@ -11,7 +11,7 @@ test('aaa', async ({  }) => {
     const context = await browser.newContext({ recordVideo: { dir: 'videos/' } });
     const page = await context.newPage();
     try {
-        console.log("Test başladı...");await page.goto('http://localhost:5173/dashboard');
-await customClick(customLocator(page, '//*[@id="app"]/div/div/div[4]/div[3]/button'), 'Click Button', page);
+        console.log("Test başladı...");await page.goto('https://app.heybooster.ai/auth/login?redirect=/');
+await customClick(customLocator(page, 'email'), 'Click Button', page);
 await context.close(); 
         console.log("Test başarılı bir şekilde bitti ...");} catch (error) { console.error("Hata:", error); throw error; } });
